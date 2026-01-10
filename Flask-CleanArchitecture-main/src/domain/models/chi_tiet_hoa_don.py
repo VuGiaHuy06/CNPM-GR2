db3e03778c9d72afe52a48645b88d51ab946d395
VuGiaHuy06 <lockdown2k6@gmail.com>
2026-01-10 16:40:00 +0700

    Delete Flask-CleanArchitecture-main/src/domain/models directory

--- Flask-CleanArchitecture-main/src/domain/models/chi_tiet_hoa_don.py
+++
@@ -1,7 +0,0 @@
-class ChiTietHoaDon:
-    def __init__(self, ID_CTHD: int = None, ID_HD: int = None, ID_MonAn: int = None, SoLuong: int = 0, VAT: float = 0.0):
-        self.ID_CTHD = ID_CTHD
-        self.ID_HD = ID_HD
-        self.ID_MonAn = ID_MonAn
-        self.SoLuong = SoLuong
-        self.VAT = VAT
--- Flask-CleanArchitecture-main/src/domain/models/hoa_don.py
+++
@@ -1,9 +0,0 @@
-class HoaDon:
-    def __init__(self, ID_HD: int = None, ID_DH: int = None, ID_Voucher: int = None, 
-                 ID_BanAn: int = None, NgayInHoaDon: str = "", TongTien: float = 0.0):
-        self.ID_HD = ID_HD
-        self.ID_DH = ID_DH
-        self.ID_Voucher = ID_Voucher
-        self.ID_BanAn = ID_BanAn
-        self.NgayInHoaDon = NgayInHoaDon
-        self.TongTien = TongTien
