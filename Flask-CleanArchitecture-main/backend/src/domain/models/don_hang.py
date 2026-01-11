777a87395140f3101f67132f2df32320b70eafbb
mynhd1575 <mynhd1575@ut.edu.vn>
2026-01-11 16:35:58 +0700

    domain-models

---
+++ Flask-CleanArchitecture-main/backend/src/domain/models/don_hang.py
@@ -0,0 +1,6 @@
+class DonHang:
+    def __init__(self, id_dh: int = None, tien_mon_an: float = 0.0, so_luong_mon_an: int = 0, id_kh: int = None):
+        self.id_dh = id_dh
+        self.tien_mon_an = tien_mon_an
+        self.so_luong_mon_an = so_luong_mon_an
+        self.id_kh = id_kh
