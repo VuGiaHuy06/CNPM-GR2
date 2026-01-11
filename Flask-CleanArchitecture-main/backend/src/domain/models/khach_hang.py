a0c0c9077810400391475c56ed9134be94c96f51
mynhd1575 <mynhd1575@ut.edu.vn>
2026-01-11 16:40:06 +0700

    domain-models

--- Flask-CleanArchitecture-main/backend/src/domain/models/khach_hang.py
+++
@@ -1,7 +0,0 @@
-class KhachHang:
-    def __init__(self, id_kh: int = None, ten_kh: str = "", so_dien_thoai: str = "", email: str = "", id_nd: int = None):
-        self.id_kh = id_kh
-        self.ten_kh = ten_kh
-        self.so_dien_thoai = so_dien_thoai
-        self.email = email
-        self.id_nd = id_nd
