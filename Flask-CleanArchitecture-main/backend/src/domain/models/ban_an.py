14d7f501ac21e060e60d4d4eba50c2dc63e54a93
mynhd1575 <mynhd1575@ut.edu.vn>
2026-01-11 16:41:55 +0700

    domain-models

---
+++ Flask-CleanArchitecture-main/backend/src/domain/models/ban_an.py
@@ -0,0 +1,6 @@
+class BanAn:
+    def __init__(self, id_banan=None, ten_ban="", trang_thai="", id_nh=None):
+        self.id_banan = id_banan
+        self.ten_ban = ten_ban
+        self.trang_thai = trang_thai
+        self.id_nh = id_nh
