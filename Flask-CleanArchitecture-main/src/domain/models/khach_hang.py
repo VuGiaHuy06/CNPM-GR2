c79f66aace9f36a8af02fd188ecd5e4441b3dd1c
VuGiaHuy06 <lockdown2k6@gmail.com>
2026-01-11 15:32:32 +0700

    Delete Flask-CleanArchitecture-main directory

--- Flask-CleanArchitecture-main/src/domain/models/khach_hang.py
+++
@@ -1,8 +0,0 @@
-class KhachHang:
-    def __init__(self, id_kh: int, ten_kh: str, sdt: str, email: str, id_nd=None):
-        self.id_kh = id_kh
-        self.ten_kh = ten_kh
-        self.sdt = sdt
-        self.email = email
-        self.id_nd = id_nd
-
--- Flask-CleanArchitecture-main/src/domain/models/nha_hang.py
+++
@@ -1,5 +0,0 @@
-class NhaHang:
-    def __init__(self, id: int, ten: str, dia_chi: str):
-        self.id = id
-        self.ten = ten
-        self.dia_chi = dia_chi
