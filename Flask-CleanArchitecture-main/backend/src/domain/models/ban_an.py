534b9b11a7ff36192804020f1f8b3734f839531c
VuGiaHuy06 <lockdown2k6@gmail.com>
2026-02-01 21:07:17 +0700

    Delete Flask-CleanArchitecture-main/backend directory

--- Flask-CleanArchitecture-main/backend/src/domain/models/ban_an.py
+++
@@ -1,6 +0,0 @@
-class BanAn:
-    def __init__(self, id_banan=None, ten_ban="", trang_thai="", id_nh=None):
-        self.id_banan = id_banan
-        self.ten_ban = ten_ban
-        self.trang_thai = trang_thai
-        self.id_nh = id_nh
