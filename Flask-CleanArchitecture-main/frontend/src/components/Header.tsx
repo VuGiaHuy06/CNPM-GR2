a70e71fdf05204a5d14d2bf7a56ad8aa2e373574
VuGiaHuy06 <lockdown2k6@gmail.com>
2026-02-01 21:07:25 +0700

    Delete Flask-CleanArchitecture-main/frontend directory

--- Flask-CleanArchitecture-main/frontend/src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Header() {
-  return (
-    <header className="h-16 bg-white border-b flex items-center px-6">
-      <h2 className="font-semibold">Dashboard</h2>
-    </header>
-  )
-}
