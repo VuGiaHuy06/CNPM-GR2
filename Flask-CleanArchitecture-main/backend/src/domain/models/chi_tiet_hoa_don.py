d39216547602e33f412dee6affbde65a135cf6ed
vypt6336-svg <vypt6336@ut.edu.vn>
2026-02-01 23:16:47 +0700

    d-models

--- Flask-CleanArchitecture-main/backend/src/domain/models/chi_tiet_hoa_don.py
+++
@@ -1,6 +0,0 @@
-class BanAn:
-    def __init__(self, ID_BanAn=None, TenBan="", TrangThai="", ID_NH=None):
-        self.ID_BanAn = ID_BanAn
-        self.TenBan = TenBan
-        self.TrangThai = TrangThai
-        self.ID_NH = ID_NH
--- Flask-CleanArchitecture-main/backend/src/domain/models/mon_an.py
+++
@@ -1,9 +0,0 @@
-class MonAn:
-    def __init__(self, id_mon_an: int = None, ten_mon: str = "", gia: float = 0.0, 
-                 mo_ta: str = "", hinh_anh: str = "", id_nh: int = None):
-        self.id_mon_an = id_mon_an
-        self.ten_mon = ten_mon
-        self.gia = gia
-        self.mo_ta = mo_ta
-        self.hinh_anh = hinh_anh
-        self.id_nh = id_nh
